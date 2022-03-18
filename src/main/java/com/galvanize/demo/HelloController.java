b8a53280747c2de5341901388f56b47e55688c69
sydfink <stefani.fink@austincc.edu>
2022-03-18 10:51:57 -0500

    initial commit

---
+++ src/main/java/com/galvanize/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.galvanize.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/cats")
+    public String getCats(){
+        return "All the cats";
+    }
+}
